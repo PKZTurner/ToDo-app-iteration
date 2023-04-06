98ac2defaafbc64e8252df7acc7d2b41a523f67a
PKZTurner <45811822+PKZTurner@users.noreply.github.com>
2023-04-05 21:44:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,50 @@
 // Data containers: forms, user inputs, list items, and clear button
-const formField = document.querySelector("#user-todo");
-const todoListItems = document.querySelector("#todo-list");
-const clearButton = document.querySelector("#clear-button");
-const inputField = document.querySelector("#new-to-do");
+let formField = document.querySelector("#user-todo");
+let todoListItems = document.querySelector("#todo-list");
+let button = document.querySelector("#clear-button");
+let listItem = document.querySelector("#new-to-do");
 let todosArray = [];
 
 // Load existing todos from local storage or create an empty array
-todosArray = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
+todosArray = localStorage.getItem("todos")
+  ? JSON.parse(localStorage.getItem("todos"))
+  : [];
 
 // Event listener for the form element in HTML
 formField.addEventListener("submit", function (e) {
   e.preventDefault();
-  if (inputField.value.trim() !== "") {
-    todosArray.push({ task: inputField.value.trim(), completed: false });
+  if (listItem.value !== "" && isNaN(listItem.value)) {
+    todosArray.push(listItem.value);
     // Updating local storage with todo items
     localStorage.setItem("todos", JSON.stringify(todosArray));
-    todoMaker(inputField.value.trim(), todosArray.length - 1);
-    inputField.value = ""; // Clear the input field after submitting
+    todoMaker(listItem.value);
+    listItem.value = ""; // Clear the input field after submitting
   }
 });
 
 // Function that creates a new todo item and appends it to the list
-const todoMaker = function (todo, index) {
-  const userToDoEntry = document.createElement("li");
-  userToDoEntry.textContent = todo.task;
-  if (todo.completed) {
-    userToDoEntry.classList.add("completed");
-  }
-  userToDoEntry.addEventListener("click", function () {
-    todo.completed = !todo.completed;
-    localStorage.setItem("todos", JSON.stringify(todosArray));
-    userToDoEntry.classList.toggle("completed");
+let todoMaker = function (todos) {
+  let userToDoEntry = document.createElement("li");
+  userToDoEntry.textContent = todos;
+  
+  // Add a click event listener to strike out todo items when clicked
+  userToDoEntry.addEventListener("click", function() {
+    userToDoEntry.style.textDecoration = "line-through";
   });
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Delete";
-  deleteButton.addEventListener("click", function () {
-    todosArray.splice(index, 1);
-    localStorage.setItem("todos", JSON.stringify(todosArray));
-    userToDoEntry.remove();
-  });
-  userToDoEntry.appendChild(deleteButton);
+  
   todoListItems.appendChild(userToDoEntry);
 };
 
 // Clear button event listener
-clearButton.addEventListener("click", function () {
+button.addEventListener("click", function () {
   while (todoListItems.firstChild) {
     todoListItems.removeChild(todoListItems.firstChild);
   }
   // Clear local storage when clear button is clicked
   localStorage.removeItem("todos");
-  todosArray = [];
 });
 
 // Load existing todos from local storage and create list items for them
 for (let i = 0; i < todosArray.length; i++) {
-  todoMaker(todosArray[i], i);
+  todoMaker(todosArray[i]);
 }
